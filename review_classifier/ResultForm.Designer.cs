316cf64c7518bfbe2004b09f2f7c9f74f09a22a6
Андрій Полтава <poltava@dlit.dp.ua>
2021-12-13 19:29:41 +0200

    feat: call all js

--- review_classifier/ResultForm.Designer.cs
+++ review_classifier/ResultForm.Designer.cs
@@ -32,10 +32,12 @@ namespace review_classifier
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(72, 60);
             this.comboBox1.Name = "comboBox1";
@@ -62,12 +64,24 @@ namespace review_classifier
             this.listBox1.TabIndex = 2;
             this.listBox1.Visible = false;
             // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(612, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 63);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "model training";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ResultForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1207, 669);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
@@ -79,6 +93,7 @@ namespace review_classifier
             this.Name = "ResultForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Shown += new System.EventHandler(this.ResultForm_Shown);
             this.ResumeLayout(false);
 
         }
@@ -88,5 +103,6 @@ namespace review_classifier
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- review_classifier/ResultForm.cs
+++ review_classifier/ResultForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,12 @@ namespace review_classifier
 {
     public partial class ResultForm : Form
     {
+        string row;
+        List<string> categories = new List<string>(),
+            apps = new List<string>(),
+            reviews = new List<string>(),
+            res = new List<string>();
+
         public ResultForm()
         {
             InitializeComponent();
@@ -21,41 +28,132 @@ namespace review_classifier
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listBox1.Visible = true;
+            switch (listBox1.Visible)
+            {
+                case true:
+                    SendRequest("getReviews", comboBox1.Text + " 20");
+                    button2.Enabled = true;
+
+                    if (row.Contains("Error"))
+                    {
+                        MessageBox.Show(row);
+                        return;
+                    }
+
+                    dynamic stuf = JsonConvert.DeserializeObject(row);
+                    foreach (var item in stuf)
+                        reviews.Add(item.ToString());
+
+                    //TODO: запись в бд
+
+                    break;
+                case false:
+                    listBox1.Visible = true;
+
+                    SendRequest("getApps", comboBox1.Text);
+
+                    if (row.Contains("Error"))
+                    {
+                        MessageBox.Show(row);
+                        return;
+                    }
+
+                    dynamic stu = JsonConvert.DeserializeObject(row);
+                    foreach (var item in stu)
+                    {
+                        apps.Add(item.ToString());
+                        listBox1.Items.Add(item.ToString());
+                    }
+
+                    listBox1.SelectedIndex = 0;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> res = new List<string>();
+            StartPython(
+            row,
+            "d", "api_train");
+
+            MessageBox.Show(res[0]);
+        }
+
+        private void ResultForm_Shown(object sender, EventArgs e)
+        {
+            SendRequest("getCategories", "");
+
+            if (row.Contains("Error"))
+            {
+                MessageBox.Show(row);
+                return;
+            }
+
+            dynamic stuf = JsonConvert.DeserializeObject(row);
+            foreach (var item in stuf)
+            {
+                categories.Add(item.ToString());
+                comboBox1.Items.Add(item);
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void SendRequest(string filename, string arguments)
+        {
+            string result;
+            ProcessStartInfo start = new ProcessStartInfo();
+
+            string curDir = Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = Directory.GetParent(curDir);
+            DirectoryInfo directoryInfo2 = Directory.GetParent(directoryInfo.FullName);
+            start.FileName = @"C:\Program Files\nodejs\node.exe";
+            string path = directoryInfo2.FullName + @"\scrapper\" + filename + ".js";
+
+            start.Arguments = string.Format("{0} {1}", path, arguments);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+                using (StreamReader reader = process.StandardOutput)
+                    result = reader.ReadToEnd();
+            // node 1.js 1 2 3 4 - запуск 1.js с аргументами 1, 2 , 3,4
+
+            dynamic stuff = JsonConvert.DeserializeObject(result);
+
+            if (stuff.data == null)
+                row = "Error: " + stuff.error.ToString();
+            else row = stuff.data.ToString();
+        }
+
+        private void StartPython(string row, string letter, string file)
+        {
+            listBox1.Items.Clear();
 
             ProcessStartInfo start = new ProcessStartInfo();
 
             string curDir = Directory.GetCurrentDirectory();
             DirectoryInfo directoryInfo = Directory.GetParent(curDir);
             DirectoryInfo directoryInfo2 = Directory.GetParent(directoryInfo.FullName);
-            // TODO: нодовский интерпретатор
-            start.FileName = @"C:\Program Files\nodejs\node.exe";
-            string path = directoryInfo2.FullName + @"\scrapper\1.js";
-            //start.FileName = path;
+            start.FileName = directoryInfo2.FullName + @"\analytics\venv\Scripts\python.exe";
+            string path = directoryInfo2.FullName + @"\analytics\" + file + ".py";
 
-            start.Arguments = string.Format("{0} {1}", path, 12);
+            start.Arguments = string.Format("{0} -{2} \"{1}\"", path, row, letter);
+            // -c - строка из текстового поля
+            // -р - путь к файлу абсолютный
+            // -d - данные от скрипта Димы
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
-            using (Process process = Process.Start(start))// node 1.js 1 2 3 4 - запуск 1.js с аргументами 1, 2 , 3,4
-            {
-                using (StreamReader reader = process.StandardOutput)
-                {
-                    string result = reader.ReadToEnd();
-                    MessageBox.Show(result);
-                }
-            }
+            string result;
+            using (Process process = Process.Start(start))
+            using (StreamReader reader = process.StandardOutput)
+                result = reader.ReadToEnd();
 
-            //using (FileStream fs = new FileStream("file.js", FileMode.Open))
-            //{
-            //    JintEngine js = JintEngine.Load(fs);
-            //    object result = js.Run("return status;");
-            //    Console.WriteLine(result);
-            //}
-        }
+            dynamic stuff = JsonConvert.DeserializeObject(result);
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            
+            if (stuff.data == null)
+                res.Add("Error: " + stuff.error.ToString());
+            else res.Add(stuff.data.ToString());
         }
     }
 }
