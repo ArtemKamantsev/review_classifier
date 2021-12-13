c1ee36803e563a5be66e2d11a442caefe80b99c1
Андрій Полтава <poltava@dlit.dp.ua>
2021-12-13 20:00:18 +0200

    feat: unenabling forms

--- review_classifier/StartForm.Designer.cs
+++ review_classifier/StartForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace review_classifier
 {
-    partial class ResultForm
+    partial class StartForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,81 +29,54 @@ namespace review_classifier
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(72, 60);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(479, 33);
-            this.comboBox1.TabIndex = 0;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(612, 44);
+            this.button1.Location = new System.Drawing.Point(12, 266);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(279, 63);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "download";
+            this.button1.Size = new System.Drawing.Size(245, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AdminForm";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(72, 126);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(479, 504);
-            this.listBox1.TabIndex = 2;
-            this.listBox1.Visible = false;
-            // 
             // button2
             // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(612, 113);
+            this.button2.Location = new System.Drawing.Point(317, 266);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(279, 63);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "model training";
+            this.button2.Size = new System.Drawing.Size(245, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "UserForm";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // ResultForm
+            // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(1207, 669);
+            this.ClientSize = new System.Drawing.Size(574, 311);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
             this.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "ResultForm";
+            this.Name = "StartForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ResultForm_FormClosing);
-            this.Shown += new System.EventHandler(this.ResultForm_Shown);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StartForm_FormClosing);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
     }
 }
---
+++ review_classifier/StartForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace review_classifier
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AdminForm form = new AdminForm();
+            form.Show();
+            Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResultForm form = new ResultForm();
+            form.Show();
+            Hide();
+        }
+
+        private void StartForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
